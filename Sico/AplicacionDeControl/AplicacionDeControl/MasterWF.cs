7b3f47f2355a5319a6c265747e8391fc4dd6a6c6
limoli <limoli@LIMOLI-PC>
2020-01-02 12:56:05 -0300

    upd: Se comienzo el desarrollo.

---
+++ Sico/AplicacionDeControl/AplicacionDeControl/MasterWF.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicacionDeControl
+{
+    public partial class MasterWF : Form
+    {
+        public MasterWF()
+        {
+            InitializeComponent();
+        }
+
+        private void MasterWF_Load(object sender, EventArgs e)
+        {
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+        }
+    }
+}
